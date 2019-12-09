a9f39b57cb44601c152e49fb7865823d411864c7
lmurdock12 <44686596+lmurdock12@users.noreply.github.com>
2019-12-09 00:19:22 -0600

    getting virenv packages for pickle file

--- app_data/jobs/triggered/pick/run.py
+++ app_data/jobs/triggered/pick/run.py
@@ -1,4 +1,8 @@
 
+import sys
+site_packages = "D:\\\\home\\site\\wwwroot\\env\\Lib\\site-packages"
+sys.path.append(site_packages)
+
 import pandas as pd
 import numpy as numpy
 from sklearn import tree
